1c315b1c5bd4859998fc0179df0c79f7b8267edc
donovan-rdz <dvnale.rdzmac@gmail.com>
2026-03-09 09:39:31 -0600

    Update 04_car_class.py

--- beginner/classes/04_car_class.py
+++ beginner/classes/04_car_class.py
@@ -20,13 +20,12 @@ class Car:
         
     def brake(self, decrease):
         self.speed -= decrease
-        if self.speed < 0:
-            self.speed = 0
         print(f"{self.brand} slowed down to {self.speed} km/h.")
 
     def __str__(self):
         return f"{self.brand} going at {self.speed} km/h"
         
-c1 = Car("Toyota", 100)
-
+c1 = Car("Toyota", 0)
+c1.accelerate(80)  # Output: Toyota accelerated to 80 km/h.
+c1.brake(100)       # Output: Toyota slowed down to 0 km/h.
 print(c1)  # Output: Toyota going at 0 km/h
